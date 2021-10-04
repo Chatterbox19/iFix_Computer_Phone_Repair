d28149be6711272fdb5ade5d209ff70cf7ff1db5
Chatterbox19 <85134713+Chatterbox19@users.noreply.github.com>
2021-10-04 19:36:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iFix_Computer_Phone_Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
