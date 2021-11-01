2934844273d2116a8dcf13d63cdf79214d96258d
Chatterbox19 <85134713+Chatterbox19@users.noreply.github.com>
2021-11-01 13:57:06 -0400

    Getting there
    Building up classes

---
+++ Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iFix_Computer_Phone_Repair
+{
+    class Customer
+    {
+        public string Name;
+        public string Device;
+        public double Phone;
+
+        public string getData()
+        {
+            return Device;
+
+
+        }
+
+        public double getDataOne()
+        {
+            return Phone;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace iFix_Computer_Phone_Repair
 {
@@ -6,7 +10,19 @@ namespace iFix_Computer_Phone_Repair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Customer> lstCustomer = new List<Customer>();
+            lstCustomer.Add(new Customer());
+            lstCustomer[0].Name = "Andrew";
+            lstCustomer[0].Device = "Pixel";
+            lstCustomer[0].Phone = 8148538955;
+
+            foreach (Customer oCust in lstCustomer)
+            {
+                Console.WriteLine(oCust.getData());
+                Console.WriteLine(oCust.getDataOne());
+            }
+            Console.WriteLine();
+
         }
     }
 }
